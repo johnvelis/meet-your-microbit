d64d9e7e534a902721d03875fb21ba3f39a5be2d
John E Velis <john@johnvelis.com>
2023-08-23 14:15:03 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,80 @@
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (input.lightLevel() < 50) {
+        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+        basic.showLeds(`
+            . . # # .
+            . . . # #
+            . . . # #
+            . . . # #
+            . . # # .
+            `)
+    } else {
+        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    }
+    basic.pause(5000)
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+    basic.clearScreen()
+    basic.pause(100)
+    basic.showString("Hello!")
+})
+input.onGesture(Gesture.Shake, function () {
+    music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+    basic.showIcon(IconNames.Surprised)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+basic.showIcon(IconNames.Happy)
